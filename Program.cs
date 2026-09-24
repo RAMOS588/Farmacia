0e3bdd5a7d5e0f871637e938c69fb6986eefe976
RAMOS588 <73040527@continental.edu.pe>
2026-09-24 11:45:58 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,42 +1,44 @@
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
     {
-        options.AddDefaultPolicy( policity =>
-            {
-                policity
-                    .AllowAnyOrigin()
-                    .AllowAnyHeader()
-                    .AllowAnyMethod()
-            }
-        )
-    }
-)
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-var app = builder.Build();
-
-app.MapGet("/",() =>
+app.MapGet("/", () =>
 {
     return "API Farmacia funcionando";
 });
 
-app.MapGet("/api/farmacia",() =>
+app.MapGet("/api/farmacia", () =>
 {
     return Results.Ok(new[]
     {
-        new{
-            id=1,
-            codigo="P001",
-            nombre="Amoxicilina",
+        new
+        {
+            id = 1,
+            codigo = "P001",
+            nombre = "Amoxicilina"
         },
-        new{
-            id=2,
-            codigo="P002",
-            nombre="Partacetamol",
+        new
+        {
+            id = 2,
+            codigo = "P002",
+            nombre = "Paracetamol"
         }
     });
 });
 
-var port = Environment.GetEnvironmentVariable("Port")??"10000",
-app.Run($"http://0.0.0.0:(port)");
+var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
+
+app.Run($"http://0.0.0.0:{port}");
+
